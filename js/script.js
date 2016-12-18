ed86d17137369fa19349ff4c405e37c01d7ff943
pauliemyers <pam528@nyu.edu>
2016-12-18 18:20:34 -0500

    All menu pages loaded
    Two navigation bars implemented. All I need to make before submitting
    tonight are modals to view selected content or scripts to change the DOM
    after selecting an icon. After submitting, I will work to make content
    more responsive and then alter the subtitles of each page.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
+    $('#navigationhome').hide();
     $('#navigation').hide();
-    $('myershome').hide();
+    $('#myershome').hide();
+    $('#myers').hide();
+    $('#bottomnav').hide();
     var topOfContent = $('#content').offset().top;
         $(window).scroll(function () {
             if ($(window).scrollTop() > topOfContent) {
+                $('#navigationhome').fadeIn("fast");
                 $('#navigation').fadeIn("fast");
                 $('#myershome').fadeIn("fast");
+                $('#myers').fadeIn("fast");
+                $('#backbutton1').fadeOut("fast");
+                $('#bottomnav').fadeOut("fast");
             } else {
+            $('#navigationhome').fadeOut("fast");
             $('#navigation').fadeOut("fast");
             $('#myershome').fadeOut("fast");
+            $('#myers').fadeOut("fast");
+            $('#backbutton1').fadeIn("fast");
             }
         });
 });
 
 $('#myershome').click(function () {
-    $('body,html').animate({scrollTop:0},500);
+    $('body,html').animate({scrollTop:0},300);
+});
+
+$('#backbutton1').click(function () {
+    $('#bottomnav').fadeIn("fast");
 });
